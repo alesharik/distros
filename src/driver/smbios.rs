60223c96cc02f986bff27ed078e34fb0a0b889e0
Aleksei Arsenev <alesharikReserv@yandex.ru>
2021-07-31 11:16:37 +0300

    Some work on smbios

--- src/driver/smbios.rs
+++ src/driver/smbios.rs
@@ -1,6 +1,10 @@
 use x86_64::{PhysAddr, VirtAddr};
-use smbioslib::MemoryMapper;
+use smbioslib::*;
 use crate::memory;
+use crate::flow::{Message, ContentProvider};
+use core::fmt::{Debug, Formatter};
+use core::marker::PhantomData;
+use core::ops::Deref;
 
 struct MemoryMapperImpl {}
 
@@ -11,9 +15,58 @@ impl MemoryMapper for MemoryMapperImpl {
     }
 }
 
+pub struct SMBiosMessage<T> where for<'a> T: SMBiosStruct<'a> + Send + Sync {
+    undef_struct: UndefinedStruct,
+    typ: PhantomData<T>
+}
+
+// impl<T> Deref for SMBiosMessage<T> where for<'a> T: SMBiosStruct<'a> + Send + Sync {
+//     type Target = T;
+//
+//     fn deref(&self) -> &Self::Target {
+//         &Self::Target::new(&self.undef_struct)
+//     }
+// }
+
+impl<T> Debug for SMBiosMessage<T> where for<'a> T: SMBiosStruct<'a> + Debug + Send + Sync {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{:?}", self.deref())
+    }
+}
+
+impl<T> Message for SMBiosMessage<T> where for<'a> T: SMBiosStruct<'a> + Debug + Send + Sync {}
+
+macro_rules! wrap {
+    ($struct:ident, $var:ident) => {
+        {
+            ContentProvider::new($struct::new(&$var.parts().clone()))
+        }
+    };
+}
+
+fn define_item(item: DefinedStruct<'_>) {
+    if let DefinedStruct::SystemReset(reset) = item {
+        // let msg = SMBiosMessage::<SMBiosSystemReset<'_>> {
+        //     undef_struct: reset.parts().clone(),
+        //     typ: PhantomData::<SMBiosSystemReset>::default(),
+        // };
+        // let provider = ContentProvider::new(msg);
+    }
+}
+
 pub fn init() {
+    info!("Setting up");
     let mut mapper = MemoryMapperImpl {};
-    let t1 = smbioslib::table_load_from_device(&mut mapper).unwrap();
-    let test = format!("{:#?}", t1);
-    info!("test");
+    let data = match smbioslib::table_load_from_device(&mut mapper) {
+        Ok(data) => data,
+        Err(e) => {
+            info!("Failed with error {:?}", e);
+            return;
+        }
+    };
+    let table: DefinedStructTable<'_> = data.iter().collect();
+    for item in table.into_iter() {
+        define_item(item)
+    }
+    info!("Setup complete");
 }
