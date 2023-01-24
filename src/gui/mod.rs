e243a9777b7e0dd798e3562cde004209bf588405
Aleksei Arsenev <alesharik4@gmail.com>
2023-01-25 01:19:37 +0300

    feat: implement TextDisplay gui module

---
+++ src/gui/mod.rs
@@ -0,0 +1,3 @@
+mod text_display;
+
+pub use text_display::TextDisplay;
---
+++ src/gui/text_display.rs
@@ -0,0 +1,129 @@
+use core::{fmt, ptr};
+use font_constants::BACKUP_CHAR;
+use noto_sans_mono_bitmap::{
+    get_raster, get_raster_width, FontWeight, RasterHeight, RasterizedChar,
+};
+use crate::driver::gpu::{FrameBuffer, Gpu, Rgb};
+
+/// Additional vertical space between lines
+const LINE_SPACING: usize = 2;
+/// Additional horizontal space between characters.
+const LETTER_SPACING: usize = 0;
+
+/// Padding from the border. Prevent that font is too close to border.
+const BORDER_PADDING: usize = 1;
+
+/// Constants for the usage of the [`noto_sans_mono_bitmap`] crate.
+mod font_constants {
+    use super::*;
+
+    /// Height of each char raster. The font size is ~0.84% of this. Thus, this is the line height that
+    /// enables multiple characters to be side-by-side and appear optically in one line in a natural way.
+    pub const CHAR_RASTER_HEIGHT: RasterHeight = RasterHeight::Size16;
+
+    /// The width of each single symbol of the mono space font.
+    pub const CHAR_RASTER_WIDTH: usize = get_raster_width(FontWeight::Regular, CHAR_RASTER_HEIGHT);
+
+    /// Backup character if a desired symbol is not available by the font.
+    /// The '�' character requires the feature "unicode-specials".
+    pub const BACKUP_CHAR: char = '�';
+
+    pub const FONT_WEIGHT: FontWeight = FontWeight::Regular;
+}
+
+/// Returns the raster of the given char or the raster of [`font_constants::BACKUP_CHAR`].
+fn get_char_raster(c: char) -> RasterizedChar {
+    fn get(c: char) -> Option<RasterizedChar> {
+        get_raster(
+            c,
+            font_constants::FONT_WEIGHT,
+            font_constants::CHAR_RASTER_HEIGHT,
+        )
+    }
+    get(c).unwrap_or_else(|| get(BACKUP_CHAR).expect("Should get raster of backup char."))
+}
+
+/// Allows logging text to a pixel-based framebuffer.
+pub struct TextDisplay<F: FrameBuffer> {
+    fb: F,
+    x_pos: usize,
+    y_pos: usize,
+}
+
+impl<F: FrameBuffer> TextDisplay<F> {
+    /// Creates a new logger that uses the given framebuffer.
+    pub fn new(framebuffer: F) -> Self {
+        let mut term = Self {
+            fb: framebuffer,
+            x_pos: 0,
+            y_pos: 0,
+        };
+        term.clear();
+        term
+    }
+
+    fn newline(&mut self) {
+        self.y_pos += font_constants::CHAR_RASTER_HEIGHT.val() + LINE_SPACING;
+        self.carriage_return()
+    }
+
+    fn carriage_return(&mut self) {
+        self.x_pos = BORDER_PADDING;
+    }
+
+    /// Erases all text on the screen. Resets `self.x_pos` and `self.y_pos`.
+    pub fn clear(&mut self) {
+        self.x_pos = BORDER_PADDING;
+        self.y_pos = BORDER_PADDING;
+        self.fb.clear();
+    }
+
+    fn width(&self) -> usize {
+        self.fb.width()
+    }
+
+    fn height(&self) -> usize {
+        self.fb.height()
+    }
+
+    /// Writes a single char to the framebuffer. Takes care of special control characters, such as
+    /// newlines and carriage returns.
+    fn write_char(&mut self, c: char) {
+        match c {
+            '\n' => self.newline(),
+            '\r' => self.carriage_return(),
+            c => {
+                let new_xpos = self.x_pos + font_constants::CHAR_RASTER_WIDTH;
+                if new_xpos >= self.width() {
+                    self.newline();
+                }
+                let new_ypos =
+                    self.y_pos + font_constants::CHAR_RASTER_HEIGHT.val() + BORDER_PADDING;
+                if new_ypos >= self.height() {
+                    self.clear();
+                }
+                self.write_rendered_char(get_char_raster(c));
+            }
+        }
+    }
+
+    /// Prints a rendered char into the framebuffer.
+    /// Updates `self.x_pos`.
+    fn write_rendered_char(&mut self, rendered_char: RasterizedChar) {
+        let raster = rendered_char.raster();
+        self.fb.write_monochrome_pixels(self.x_pos, self.y_pos, raster);
+        self.x_pos += rendered_char.width() + LETTER_SPACING;
+    }
+}
+
+unsafe impl<F: FrameBuffer> Send for TextDisplay<F> {}
+unsafe impl<F: FrameBuffer> Sync for TextDisplay<F> {}
+
+impl<F: FrameBuffer> fmt::Write for TextDisplay<F> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars() {
+            self.write_char(c);
+        }
+        Ok(())
+    }
+}
