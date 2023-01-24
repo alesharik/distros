172c4a0aee8dda2997878f89f150df2b9d10f0cc
Aleksei Arsenev <alesharik4@gmail.com>
2023-01-25 02:58:30 +0300

    feat: set ss and ds in gdt

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,14 +1,16 @@
 use crate::kblog;
 use lazy_static::lazy_static;
+use x86_64::registers::segmentation::{CS, DS, Segment, SS};
 use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector};
 use x86_64::structures::tss::TaskStateSegment;
-use x86_64::VirtAddr;
+use x86_64::{PrivilegeLevel, VirtAddr};
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
 struct Selectors {
     code_selector: SegmentSelector,
     tss_selector: SegmentSelector,
+    data_selector: SegmentSelector,
 }
 
 lazy_static! {
@@ -27,23 +29,26 @@ lazy_static! {
         let mut gdt = GlobalDescriptorTable::new();
         let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
         let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+        let data_selector = gdt.add_entry(Descriptor::kernel_data_segment());
         (
             gdt,
             Selectors {
                 code_selector,
                 tss_selector,
+                data_selector
             },
         )
     };
 }
 
 pub fn init_gdt() {
-    use x86_64::instructions::segmentation::set_cs;
     use x86_64::instructions::tables::load_tss;
 
     GDT.0.load();
     unsafe {
-        set_cs(GDT.1.code_selector);
+        CS::set_reg(GDT.1.code_selector);
+        SS::set_reg(GDT.1.data_selector);
+        DS::set_reg(GDT.1.data_selector);
         load_tss(GDT.1.tss_selector);
     }
     kblog!("GDT", "GDT and TSS table loaded, double fault IST ready");
